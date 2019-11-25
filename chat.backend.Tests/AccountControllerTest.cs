0984b388f50695629ae936b2a723fd9ebd3c7232
Vacuum113 <dimentr12170@gmail.com>
2019-11-25 20:13:48 +0500

    add new connection string with sql auth

---
+++ chat.backend.Tests/AccountControllerTest.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+
+namespace chat.backend.Tests
+{
+    public class AccountControllerTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
